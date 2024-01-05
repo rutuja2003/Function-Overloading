8d4c443f0c613e4be17729673001348ab74e6f68
rutuja2003 <rutujasathe2003@gmail.com>
2024-01-05 12:18:08 +0530

    First Commit.

---
+++ src/Area.java
@@ -0,0 +1,14 @@
+
+public class Area {
+
+	public void calcArea(float r) {
+		float ca=3.14f*r*r;
+		System.out.println("Area of Circle: "+ca);
+	}
+	
+	public void calcArea(float b, float h) {
+		float ta=0.5f*b*h;
+		System.out.println("Area of triangle:"+ta);
+		
+	}
+}
---
+++ src/AreaMain.java
@@ -0,0 +1,11 @@
+
+public class AreaMain {
+
+	public static void main(String[] args) {
+	      Area a= new Area();
+	      a.calcArea(2.4f);
+	      a.calcArea(2.4f,3.3f);
+
+	}
+
+}
